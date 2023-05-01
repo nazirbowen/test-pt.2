a330256e526684600ad52d6fa2336225e5ad8580
nazirbowen <108888651+nazirbowen@users.noreply.github.com>
2023-05-01 06:13:05 -0700

    Add files via upload

---
+++ test pt.2.py
@@ -0,0 +1,41 @@
+#1 A peice of code that is reuseable and uses a certain function when it is called 
+#2 A method operates that data and functions return or pass data
+#3 A collection of key values pairs
+#4 GitHub is a service for software development and coding. It provides the distributed version and the breaking down of code, bug tracking, and task management from how much it breaks down things.
+#5 Class are for putting code togther. Dictionaries store key values
+
+    cities=
+    'Philadelphia' :' Pennsylvania ',
+    'Detroit ':'Michagan '
+    'Houston':'Texas',
+    'Kanas City': 'Missouri',
+    'Dallas':'Texas',
+
+print(cities)
+print(len(cities))
+
+class Bank_Account:
+    def __init__(self, Account_Number, Balence, Owner_Name, ATM)
+        self.Account_Number=Account_Number 
+        self.Balence= Balence
+        self.Owner_Name=Owner_Name
+        self.ATM=ATM
+
+Code=Bank_Account:('190234229')
+
+print(Code.Account_Number)
+
+Number=Bank_Account('2,000,000')
+
+print(Number.Balence)
+
+Name= Bank_Account('Nazir Bowen')
+
+print(Money.ATM)
+
+#10 Incorrect Placing, Missing Parts of code, The structre of the code may be incorrect
+
+def function(name):
+    print('hello'+name)
+    'How are you today'
+    
